abed532f7cf6538e54e49bdcc695dcab9a19d08f
zhiminy <knightyzhao@gmail.com>
2019-09-06 10:56:37 +0800

    full project

---
+++ auto_tetris/GridPosition.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#ifndef GRIDPOSITION_CLASS
+#define GRIDPOSITION_CLASS
+
+/*
+* 类： 20 * 10 的栅栏坐标
+*/
+class GridPosition
+{
+public:
+	int x;
+	int y;
+
+	GridPosition();
+	GridPosition(int, int);
+	~GridPosition();
+	GridPosition & operator = (const GridPosition &);
+
+};
+
+#endif // !GRIDPOSITION_CLASS
